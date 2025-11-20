35779daf93c3a0d0a2fe554a96a43a93b4083224
KirisameMarisa0602 <3938314729@qq.com>
2025-11-20 19:18:24 +0800

    个人空间界面引入导航栏

--- src/pages/SelfSpace.jsx
+++ src/pages/SelfSpace.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import '../styles/selfspace/SelfSpace.css';
 import SelfspaceProfileAccordion from '../components/selfspace/SelfspaceProfileAccordion/SelfspaceProfileAccordion.jsx';
+import BannerNavbar from '../components/common/BannerNavbar.jsx';
 
 // SelfSpace 页面：左侧 25vw 手风琴资料面板 + 右侧内容区域
 export default function SelfSpace() {
   return (
-    <div className="selfspace-page" data-page="selfspace">
-      <aside className="selfspace-left-panel" aria-label="个人空间侧边栏">
-        <div className="selfspace-left-panel-inner">
-          <SelfspaceProfileAccordion panelWidth="100%" panelHeight="100%" />
-        </div>
-      </aside>
+    <>
+      <BannerNavbar />
+      <div className="selfspace-page" data-page="selfspace">
+        <aside className="selfspace-left-panel" aria-label="个人空间侧边栏">
+          <div className="selfspace-left-panel-inner">
+            <SelfspaceProfileAccordion panelWidth="100%" panelHeight="100%" />
+          </div>
+        </aside>
 
-      <main className="selfspace-right-panel" aria-label="个人空间内容区">
-        {/* 这里放自空间的主要内容模块 */}
-      </main>
-    </div>
+        <main className="selfspace-right-panel" aria-label="个人空间内容区">
+          {/* 这里放自空间的主要内容模块 */}
+        </main>
+      </div>
+    </>
   );
 }
